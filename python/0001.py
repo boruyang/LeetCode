29b767d653c7de8b8d4585113321f96e3daf0829
Boru Yang <listen82231@gmail.com>
2022-10-22 21:36:18 +0800

    Add 0001.py

---
+++ python/0001.py
@@ -0,0 +1,16 @@
+### 0001. Two Sum ###
+
+from typing import List
+
+class Solution():
+    def twoSum(self, nums: List[int], target: int) -> List[int]:        
+        seen_num = {}
+        for i, num in enumerate(nums):
+            if target - num in seen_num:
+                return [seen_num[target - num], i]
+            elif num not in seen_num:
+                seen_num[num] = i
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.twoSum([2, 7, 11, 15], 9))
